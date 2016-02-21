0292b24f65d94252640a054db9d0679b0f4c8086
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-02-21 14:31:37 +0100

    base gem boostrap

---
+++ lib/paystack.rb
@@ -0,0 +1,5 @@
+require "paystack/version"
+
+module Paystack
+  # Your code goes here...
+end
---
+++ spec/paystack_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Paystack do
+  it 'has a version number' do
+    expect(Paystack::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
